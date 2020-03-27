72ee8ddc7bf8d63afcb57be68dbea694b62e5f0d
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-03-26 21:55:51 -0700

    Intergrate file manager

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:latest
 
+RUN apk --no-cache add php7 php7-json php7-fileinfo php7-zip php7-bz2 php7-posix php7-zlib php7-phar
+
 COPY root/ /
 
 VOLUME /folders
--- root/startup.sh
+++ root/startup.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-trap "killall sleep ; exit" INT TERM
+trap "killall sleep php ; exit" INT TERM
+
+export IFM_ROOT_DIR=/folders IFM_AUTH=0 IFM_API_AJAXREQUEST=false IFM_API_CHMOD=false IFM_API_EDIT=false \
+       IFM_GUI_SHOWOWNER=false IFM_GUI_SHOWGROUP=false IFM_GUI_SHOWPERMISSIONS=false IFM_API_REMOTEUPLOAD=false
+
+/usr/bin/php -S 0:80 -t /www &
 
 sleep 2147483647d &
 wait "$!"
